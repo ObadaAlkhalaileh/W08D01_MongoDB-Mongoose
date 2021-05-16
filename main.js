0706d0416355cb612a4374492b2bfefd02a74b51
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-16 23:53:02 +0400

    getting started

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+
+app.get("/", (req, res, next) => {
+    res.json("hello from server");
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
